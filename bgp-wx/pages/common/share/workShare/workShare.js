014a4d7cf6e9c01445cdc0657481cbef5fe0a924
xiewei <814570526@qq.com>
2018-08-08 00:22:38 +0800

    消息推送

--- bgp-wx/pages/common/share/workShare/workShare.js
+++ bgp-wx/pages/common/share/workShare/workShare.js
@@ -1,4 +1,5 @@
 const app = getApp()
+var util = require('../../../utils/util.js')
 Page({
 
   /**
@@ -22,14 +23,38 @@ Page({
     //根据type的值来执行相应的点击事件
   },
   dealFormIds: function (formId) {
+    let loginUser = wx.getStorageSync("loginUser");
     let formIds = app.globalData.gloabalFomIds;//获取全局数据中的推送码gloabalFomIds数组
     if (!formIds) formIds = [];
     let data = {
+      account: loginUser.account,
       formId: formId,
       expire: parseInt(new Date().getTime() / 1000) + 604800 //计算7天后的过期时间时间戳
     }
     formIds.push(data);//将data添加到数组的末尾
     app.globalData.gloabalFomIds = formIds; //保存推送码并赋值给全局变量
+    let url = '/wx/msg/collect';
+    let method = 'post';
+
+    util.onSubmitJson(url, data, method, function (res) {
+      if (res.data.retCode != 200) {
+        util.openAlert(res.data.msg);
+      } else {
+        
+      }
+    })
+  },
+  sentMsg: function(e){
+    let url = '/wx/msg/pushMsg';
+    let method = 'post';
+
+    util.onSubmitJson(url, data, method, function (res) {
+      if (res.data.retCode != 200) {
+        util.openAlert(res.data.msg);
+      } else {
+
+      }
+    })
   },
   /**
    * 用户点击右上角分享
